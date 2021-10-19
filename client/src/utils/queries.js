d62dbc7f4b3302aee7a244b8f2e25764b7b19daa
Bri <bhilliker@gmail.com>
2021-10-19 14:30:09 -0500

    add GET_ME query

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client';
+
+// GET_ME will execute the me query set up using Apollo Server
+export const GET_ME = gql`
+    {
+        me{
+            _id
+            username
+            email
+            savedBooks{
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }    
+`;
